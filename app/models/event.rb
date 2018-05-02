7cf8212d67369eb27620120e68a2316a4d0bfe4e
RSchaubeck <richard.schaubeck@gmail.com>
2018-05-01 21:12:03 -0400

    created activerecord associations between models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ApplicationRecord
+  belongs_to :location
+  has_many :events_user
+  has_many :users, through: :events_user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :events_user
+  has_many :events, through: :events_user
 end

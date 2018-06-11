3212ce4c18baa58de2df37638d3a1928e8e3c43d
RSchaubeck <richard.schaubeck@gmail.com>
2018-06-11 14:25:49 -0400

    changed data type to datetime, and migrated db

--- db/migrate/20180429205619_create_events.rb
+++ db/migrate/20180429205619_create_events.rb
@@ -2,8 +2,8 @@ class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
       t.string :name
-      t.strftime :start
-      t.strftime :end
+      t.datetime :start
+      t.datetime :end
       t.integer :location_id
 
       t.timestamps

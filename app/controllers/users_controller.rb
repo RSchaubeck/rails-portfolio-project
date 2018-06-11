81b6b87f95784134e3c62a93e6d60a16220a8802
RSchaubeck <richard.schaubeck@gmail.com>
2018-06-11 18:31:55 -0400

    added signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,20 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    return redirect_to new_user_path unless @user.save
+    session[:user_id] = @user.id
+    redirect_to '/'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :username, :password, :password_confirmation)
   end
 
 end

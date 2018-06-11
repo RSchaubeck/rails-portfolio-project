459b842c3d71c9f62e4c04606bdd153381b1ced0
RSchaubeck <richard.schaubeck@gmail.com>
2018-06-11 18:56:21 -0400

    added signup form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,15 @@
 class SessionsController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
-
+    @user = User.find_by(username: params[:user][:username])
+    @user = @user.try(:authenticate, params[:user][:password])
+    return redirect_to new_session_path unless @user
+    session[:user_id] = @user.id
+    redirect_to "/"
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'users#index'
-
+  resources :sessions, only: [:new, :create]
   resources :users
   resources :locations
   resources :events
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

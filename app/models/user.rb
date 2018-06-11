3e2dbda050bb7c9dbf0df364e1ae1d6a00d92e3a
RSchaubeck <richard.schaubeck@gmail.com>
2018-06-11 15:44:59 -0400

    added framework for login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord::Base
   has_many :events, through: :events_user
   validates :first_name, :last_name, :username, :presence => true
   validates :username, :length => { :in => 5..20}, :unqueness => true
+  has_secure_password
 end

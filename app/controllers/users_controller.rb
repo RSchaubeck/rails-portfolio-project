e022b38e0e33624aefcf45e60bea2fb2669e7ec6
RSchaubeck <richard.schaubeck@gmail.com>
2018-06-18 17:22:54 -0400

    added flash notice for failed signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   end
 
   def create
+    flash[:notice] = "Make sure passwords match and/or email is correct."
     @user = User.new(user_params)
     return redirect_to new_user_path unless @user.save
     session[:user_id] = @user.id

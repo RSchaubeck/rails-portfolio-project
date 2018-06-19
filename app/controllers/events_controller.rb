e93492d376e8a3dd2365e8b033e5d73085386c3c
RSchaubeck <richard.schaubeck@gmail.com>
2018-06-19 17:38:27 -0400

    added event#index controller action and corresponding view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,16 @@
 class EventsController < ApplicationController
   def index
-
+    if params[:location_id]
+      location = Location.find_by(id: params[:location_id])
+        if location.nil?
+          flash[:alert] = "Location not found."
+          redirect_to locations_path
+        else
+          @events = location.events
+        end
+    else
+      @events = Event.all
+    end
   end
 
   def new

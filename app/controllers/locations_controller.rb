bc8ae2cba9aeee84fb9c8bc9f669624e5877505f
RSchaubeck <richard.schaubeck@gmail.com>
2018-06-18 21:21:35 -0400

    restricted access to locations#new for only logged in users

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,16 +1,32 @@
 class LocationsController < ApplicationController
+  before_action :require_login, only: [:new]
 
   def new
   end
 
   def create
-    @location = Location.new(params.require(:location))
+    @location = Location.new(location_params)
     return redirect_to new_location_path unless @location.save
     redirect_to location_path(@location)
   end
 
   def show
     @location = Location.find(params[:id])
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:name, :address, :city, :state, :zip_code)
+  end
+
+  def require_login
+    if session[:user_id]
+      render "locations/new"
+    else
+      flash[:error] = "You must be logged in."
+      redirect_to login_path
+    end
   end
 
 end

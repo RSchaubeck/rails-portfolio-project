4c0530673431248785400531032ee2b629ff82c7
RSchaubeck <richard.schaubeck@gmail.com>
2018-07-09 19:23:59 -0400

    added method to event model to format datetime to readable format and added link on locations show page to add events

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,8 @@ class Event < ActiveRecord::Base
   has_many :users, through: :events_user
   validates :name, :start, :end_time, :presence => true
 
+  def readable_time(time_attr)
+    time_attr.strftime('%A, %d %b %Y %l:%M %p')
+  end
+
 end

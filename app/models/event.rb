e830c25ddb49088ed346d1dba2a38d44ab71f8ed
RSchaubeck <richard.schaubeck@gmail.com>
2018-07-16 19:11:22 -0400

    removed start and end_time presence validation because user is forced to input times anyway

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
   belongs_to :location
   has_many :events_user
   has_many :users, through: :events_user
-  validates :name, :start, :end_time, presence: true
+  validates :name, presence: true
 
   def readable_time(time_attr)
     time_attr.strftime('%A, %d %b %Y %l:%M %p')

797069fa4a85d7d992e8fbe51e7cbc54364e9b32
RSchaubeck <richard.schaubeck@gmail.com>
2018-07-16 16:44:44 -0400

    Changed the logic in Lessons#create so I can implement error messages

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,24 +1,25 @@
-require 'pry'
 class LocationsController < ApplicationController
   before_action :require_login, only: [:new]
 
   def index
     @locations = Location.all
-  #  binding.pry
   end
 
   def new
+    @location = Location.new
   end
 
   def create
-    @location = Location.create(location_params)
-    return redirect_to new_location_path unless @location.save
-    redirect_to location_path(@location)
+    @location = Location.new(location_params)
+    if @location.save
+      redirect_to @location
+    else
+      render :new
+    end
   end
 
   def show
     @location = Location.find(params[:id])
-    flash[:success] = "You have successfully added this location!"
   end
 
   private
